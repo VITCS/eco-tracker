8cf7d45de0484370014e87f68e69c9fbd8efc6d2
Virtualance Technologies <35986627+VITCS@users.noreply.github.com>
2025-09-10 13:03:03 +0530

    commit

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -4,7 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
+import { initTheme } from "./lib/theme";
 
+initTheme();
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")!).render(
